6a096d48020d36a1b546898ddfb1bdd9ea0c96e1
KevinKipkemei <42734616+KevinKipkemei@users.noreply.github.com>
2023-03-23 15:16:21 +0300

    Map display, central location Kenya

--- app/map.js
+++ app/map.js
@@ -1,12 +1,26 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const map= () => {
   return (
-    <View>
-      <Text>Test Map Page</Text>
-      <Text>Another Test</Text>
+    <View style =  {styles.container}>
+      <MapView 
+        style = {styles.map}
+        initialRegion = {{latitude: 0.0236, longitude: 37.9062, latitudeDelta: 0.00000006, longitudeDelta: 8 }}
+      />
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map : {
+    width: "100%",
+    height: '100%',
+  },
+})
 
 export default map
